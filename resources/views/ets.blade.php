fae0e892554aa23c02e2bc26ead0a5c79b73958b
nastreacode <trea.hasnadaffa@gmail.com>
2023-11-06 14:42:22 +0700

    first commit

---
+++ resources/views/ets.blade.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <title>Custom Linktree</title>
+    <link rel="stylesheet" href="ets.css">
+</head>
+<body>
+    <div class="hasna-container">
+        <div class="hasna-profile">
+            <img src="logo.png" alt="Profile Picture" class="hasna-avatar">
+            <h1>@lincafe.resto</h1>
+            <h2>Family Style Restaurant</h2>
+        </div>
+
+        <div class="hasna-links">
+            <h3 class="hasna-section-header">Digital Menu</h3>
+            <a href="https://tr.ee/G7t12pZAMn" class="hasna-link-box"><img src="menu2.png" alt="Link Logo" class="hasna-link-logo"> Digital Menu</a>
+
+            <h3 class="hasna-section-header">Online Food Delivery</h3>
+            <a href="https://s.id/1THgP" class="hasna-link-box" target="_blank"><img src="gofood.png" alt="Link Logo" class="hasna-link-logo"> GoFood - Galaxy Mall Surabaya</a>
+            <a href="https://s.id/1THh3" class="hasna-link-box" target="_blank"><img src="gofood.png" alt="Link Logo" class="hasna-link-logo"> GoFood - Ciputra World Surabaya</a>
+            <a href="https://s.id/1THh5" class="hasna-link-box" target="_blank"><img src="gofood.png" alt="Link Logo" class="hasna-link-logo"> GoFood - Pakuwon Mall Surabaya</a>
+            <a href="https://s.id/1THh8" class="hasna-link-box" target="_blank"><img src="gofood.png" alt="Link Logo" class="hasna-link-logo"> GoFood - Lincafe Express</a>
+            <a href="https://s.id/1THhw" class="hasna-link-box" target="_blank"><img src="grabfood.png" alt="Link Logo" class="hasna-link-logo"> GoFood - Lincafe Express</a>
+
+            <h3 class="hasna-section-header">Contact Us</h3>
+            <a href="https://s.id/1THhC" class="hasna-link-box" target="_blank"><img src="wa.png" alt="Link Logo" class="hasna-link-logo"> Galaxy Mall Surabaya</a>
+            <a href="https://s.id/1THhE" class="hasna-link-box" target="_blank"><img src="wa.png" alt="Link Logo" class="hasna-link-logo"> Ciputra World Surabaya</a>
+            <a href="https://s.id/1THhI" class="hasna-link-box" target="_blank"><img src="wa.png" alt="Link Logo" class="hasna-link-logo"> Pakuwon Mall Surabaya</a>
+            <a href="https://s.id/1THhJ" class="hasna-link-box" target="_blank"><img src="wa.png" alt="Link Logo" class="hasna-link-logo"> Lincafe Express</a>
+        </div>
+    </div>
+</body>
+</html>
+
---
+++ resources/views/p2_index.blade.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>Hasna Daffa Ulinnuha | 5026221205</title>
+
+</head>
+
+<body bgcolor="#a5ccc3">
+    Hallo, Apa kabar? <br>
+    <h1>Hallo, Apa kabar?</h1>
+    <h5>Hallo, Apa kabar? </h5>
+    <p>Hallo, Apa kabar? </p>
+    Hallo, Apa kabar?
+    <a href="https://www.its.ac.id/">Web ITS</a>
+    <a href="https://www.detik.com/" target="_blank">Detik.com</a>
+    <a href="https://www.w3schools.com/"  target="_blank">w3schools HTML</a>
+    <br>
+    <img src="" alt="">
+    <br>
+    <img src="https://www.its.ac.id/news/wp-content/uploads/sites/2/2023/07/Tim-Going-Global-Project-GGP-pada-saat-pelaksanaan-proyek-lapangan-dalam-program-Blue-Energy-for-Blue-Economy-di-Malang.jpeg">
+</body>
+
+</html>
